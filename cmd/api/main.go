604a202ed26307b87efff72b7c48a7be9c5c5f09
aldowitzke <aldo_mwa@hotmail.com>
2021-07-19 20:13:22 -0300

    add migration files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -94,7 +94,7 @@ func openDB(cfg config) (*sql.DB, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err = db.PingContext(ctx)
+	err = db.PingContext(ctx)	
 	if err != nil {
 		return nil, err
 	}
